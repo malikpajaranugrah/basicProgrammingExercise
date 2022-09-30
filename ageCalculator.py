04858b710c9679a33b0cbabb9f9590ff4cea7fe3
malik pajar anugrah <malik.pajar_ti22@nusaputra.ac.id>
2022-09-30 16:26:07 +0700

    first commit

---
+++ ageCalculator.py
@@ -0,0 +1,7 @@
+# this created by malik pajar anugrah || 20220040128
+
+# get birth year 
+birth_year = int(input("input your birth year! : "))
+this_year = int(input("input this year! : "))
+age = this_year - birth_year
+print("Age: ",age)
