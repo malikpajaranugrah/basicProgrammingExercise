1d503d2bce92f21451ee4ff547ed14e6a6604b66
Amaradipta <muhammad.amaradipta_ti22@nusaputra.ac.id>
2022-10-07 14:25:38 +0700

    update

---
+++ session3.py
@@ -0,0 +1,9 @@
+name = "Muhammad Amaradipta"
+Class = "TI22I"
+major = "Informatics Engineering"
+quote = "y"
+age = 20
+iLikespiceFood = True
+hobbies = {"playing games, reading"}
+
+print(f"My name is: {name}\nClass: {Class}\nMajor: {major}\nQuote: {quote}\nAge: {age}\niLikespiceFood: {iLikespiceFood}\nHobbies: {hobbies}  ")
